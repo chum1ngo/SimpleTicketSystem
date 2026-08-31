38089d388f5c29aa793b20c16c9ae715a672825b
chum1ngo <jose.montecinosa@sansano.usm.cl>
2026-08-30 23:52:58 -0400

    REF: normaliza vista de comentarios

--- backend/sts/views/comments.py
+++ backend/sts/views/comments.py
@@ -8,8 +8,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from ..models import Ticket
-from ..serializers import CommentSerializer
 from ..roles import get_user_role
+from ..serializers import CommentSerializer
 
 
 @api_view(["GET", "POST"])
@@ -32,7 +32,7 @@ def ticket_comments_read_create(request, pk):
             serializer.save(
                 ticket=ticket,
                 created_by=request.user,
-                comment_type=get_user_role(request.user)
+                comment_type=get_user_role(request.user),
             )
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
